b2d5755a48220d9b4d468316c2324a5234f0e2fa
Jiaxin Miao <Jiaxin Miao>
2017-04-06 18:16:46 -0400

    4/6.0

--- src/main/resources/static/js/NavSchemaHideAndShow.js
+++
@@ -1,9 +0,0 @@
-function showls(ls){
-    var a=ls.getElementsByTagName("ul")[0];
-    a.style.display="block";
-}
-
- function nonels(ls){
-    var a=ls.getElementsByTagName("ul")[0];
-    a.style.display="none";
-}
--- src/main/resources/static/js/index.js
+++
@@ -1,5 +0,0 @@
-window.onload=function{
-	//get dom file and named box
-	var box= document.getElementById("header");
-	
-}
