6e5aa2802b0423a7c65e49c89a9c4f65986ed808
Jiaxin Miao <Jiaxin Miao>
2017-04-11 21:33:54 -0400

    4/11.11

--- src/main/java/edu/infsci2560/models/Car.java
+++ src/main/java/edu/infsci2560/models/Car.java
@@ -26,22 +26,28 @@ public class Car {
     protected Long id;
     protected String title;
     protected String carType;
+    protected String price;
+    protected String description;
 
     public Car() {
         this.id = Long.MAX_VALUE;
         this.title = null;
         this.carType = null;
+        this.price = null;
+        this.description = null;
     }
 
-    public Car(Long id, String name, String carType) {
+    public Car(Long id, String name, String carType, String price, String description) {
         this.id = id;
         this.title = name;
         this.carType = carType;
+        this.price = price;
+        this.description = description;
     }
 
     @Override
     public String toString() {
-        return "[ id=" + this.id + ", title=" + this.title + ", carType=" + this.carType + " ]";
+        return "[ id=" + this.id + ", title=" + this.title + ", carType=" + this.carType + ", price=" + this.price + ", description=" + this.description + " ]";
     }
 
     @Override
@@ -94,6 +100,20 @@ public class Car {
      */
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+     public void setPrice(String price) {
+        this.price = price;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
 }
