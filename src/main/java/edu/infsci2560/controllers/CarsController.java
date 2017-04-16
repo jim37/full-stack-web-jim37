04c411885a913d2806ba42d1e3513f9cbe0f627e
Jiaxin Miao <Jiaxin Miao>
2017-04-16 18:47:56 -0400

    4/16.9

--- src/main/java/edu/infsci2560/controllers/CarsController.java
+++ src/main/java/edu/infsci2560/controllers/CarsController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.h    ttp.ResponseEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
