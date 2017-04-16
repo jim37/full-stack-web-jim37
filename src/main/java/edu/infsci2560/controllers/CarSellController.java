0f03add72cb763b50a00af9157b205121edc598b
Jiaxin Miao <Jiaxin Miao>
2017-04-16 18:38:50 -0400

    4/16.1

---
+++ src/main/java/edu/infsci2560/controllers/CarSellController.java
@@ -0,0 +1,26 @@
+package edu.infsci2560.controllers;
+
+import edu.infsci2560.models.Car;
+import edu.infsci2560.repositories.CarRepository;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.data.domain.PageRequest;
+
+
+@Controller
+public class CarSellController {
+    @Autowired
+    private CarRepository carRepository;
+
+    @RequestMapping(value = "carsell/{carType}", method = RequestMethod.POST, consumes="application/x-www-form-urlencoded", produces = "application/json")
+    public ModelAndView carsell(@PathVariable String carType) {
+        return new ModelAndView("carsell", "carsell", repository.findAll(carType));
+    }
+
+}
